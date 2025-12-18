d68557c4a8ce4b8aaee2a8ea5939147bc7faaceb
Tarun Velagala <tarun.kumar.reddy.velagala@sap.com>
2025-12-18 22:08:56 +0530

    feat: initialize Flutter bindings and lock app orientation to portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that SystemChrome can be called
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Lock the app to portrait mode
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const MyApp());
 }
 
