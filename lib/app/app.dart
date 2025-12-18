f676a79377b9afce589055865588e2804928d696
Tarun Velagala <tarun.kumar.reddy.velagala@sap.com>
2025-12-18 22:34:47 +0530

    chore: strip material visual noise for launcher foundation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
-import 'return_zero_app.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ReturnZeroApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        useMaterial3: false,
+        splashFactory: NoSplash.splashFactory,
+        highlightColor: Colors.transparent,
+        splashColor: Colors.transparent,
+      ),
+
+      home: const LauncherHome(),
+    );
+  }
+}
+
+class LauncherHome extends StatelessWidget {
+  const LauncherHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(child: SizedBox.expand()),
+    );
   }
 }
