6de4538567cba3179779693549626d283b8db3d4
Tarun Velagala <tarun.kumar.reddy.velagala@sap.com>
2025-12-18 22:52:12 +0530

    feat: add typography system using Space Mono and Darker Grotesque

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -10,6 +10,33 @@ class App extends StatelessWidget {
 
       theme: ThemeData(
         useMaterial3: false,
+
+        textTheme: const TextTheme(
+          // Time / Numbers
+          displayLarge: TextStyle(
+            fontFamily: 'SpaceMono',
+            fontSize: 48,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),
+
+          // Stats (screen time, unlocks)
+          titleMedium: TextStyle(
+            fontFamily: 'SpaceMono',
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: Colors.white70,
+          ),
+
+          // Dynamic text / labels
+          bodyMedium: TextStyle(
+            fontFamily: 'DarkerGrotesque',
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            color: Colors.white70,
+          ),
+        ),
+
         splashFactory: NoSplash.splashFactory,
         highlightColor: Colors.transparent,
         splashColor: Colors.transparent,
