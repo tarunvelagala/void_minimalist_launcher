488311d60bc6b11e782c96dd29d2b4f1bca7b65f
Tarun Velagala <tarun.kumar.reddy.velagala@sap.com>
2025-12-22 11:49:56 +0530

    feat: implement settings controls UI with placeholder text descriptions

--- lib/features/home/widgets/settings/controls.dart
+++ lib/features/home/widgets/settings/controls.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:return_zero/core/theme/app_layout.dart';
+import 'package:return_zero/core/theme/app_typography.dart';
 
 class Controls extends StatelessWidget {
   const Controls({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        spacing: AppLayout.contentPadding(context),
+        children: [
+          Text('Controls', style: AppTypography.headlineSmall(context)),
+          Text('Swipe Left App', style: AppTypography.bodyLarge(context)),
+          Text('Swipe Right App', style: AppTypography.bodyLarge(context)),
+          Text('Double tap to lock', style: AppTypography.bodyLarge(context)),
+        ],
+      ),
+    );
   }
 }
