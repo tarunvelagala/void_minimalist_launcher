8b2543b48da7e5c75c5672daf7cacb8fbea645cf
Tarun Velagala <tarun.kumar.reddy.velagala@sap.com>
2025-12-22 22:30:23 +0530

    refactor: remove global scaleFactor and pass it directly to typography scaling, adjusting adaptive scaling values

--- lib/core/theme/app_typography.dart
+++ lib/core/theme/app_typography.dart
@@ -2,20 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 class AppTypography {
-  /// The global font size scale factor (0-7).
-  /// For now, we default to 1.0 (Middle/Normal).
-  /// In the future, this will be tied to a provider.
-  static double get scaleFactor => 1.0;
-
-  static double _scale(BuildContext context, double baseSize) {
-    // Basic adaptive scaling
+  static double _scale(
+    BuildContext context,
+    double baseSize,
+    double scaleFactor,
+  ) {
     final adaptiveScale = ResponsiveValue<double>(
       context,
       defaultValue: 1.0,
-      conditionalValues: [
-        const Condition.equals(name: MOBILE, value: 1.0),
-        const Condition.equals(name: TABLET, value: 1.2),
-        const Condition.largerThan(name: TABLET, value: 1.3),
+      conditionalValues: const [
+        Condition.equals(name: MOBILE, value: 1.0),
+        Condition.equals(name: TABLET, value: 1.1),
+        Condition.largerThan(name: TABLET, value: 1.2),
       ],
     ).value;
 
@@ -23,140 +21,140 @@ class AppTypography {
   }
 
   static TextStyle displayLarge(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 57),
+    fontSize: _scale(context, 57, 1.0),
     fontWeight: FontWeight.w800,
     height: 1.12,
     letterSpacing: -0.25,
   );
 
   static TextStyle displayMedium(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 45),
+    fontSize: _scale(context, 45, 1.0),
     fontWeight: FontWeight.w700,
     height: 1.16,
     letterSpacing: 0,
   );
 
   static TextStyle displaySmall(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 36),
+    fontSize: _scale(context, 36, 1.0),
     fontWeight: FontWeight.w700,
     height: 1.22,
     letterSpacing: 0,
   );
 
   static TextStyle headlineLarge(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 32),
+    fontSize: _scale(context, 32, 1.0),
     fontWeight: FontWeight.w600,
     height: 1.25,
     letterSpacing: 0,
   );
 
   static TextStyle headlineMedium(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 28),
+    fontSize: _scale(context, 28, 1.0),
     fontWeight: FontWeight.w600,
     height: 1.29,
     letterSpacing: 0,
   );
 
   static TextStyle headlineSmall(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 24),
+    fontSize: _scale(context, 24, 1.0),
     fontWeight: FontWeight.w600,
     height: 1.33,
     letterSpacing: 0,
   );
 
   static TextStyle titleLarge(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 22),
+    fontSize: _scale(context, 22, 1.0),
     fontWeight: FontWeight.w500,
     height: 1.27,
     letterSpacing: 0,
   );
 
   static TextStyle titleMedium(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 16),
+    fontSize: _scale(context, 16, 1.0),
     fontWeight: FontWeight.w400, // Reduced from w500 to look less bold
     height: 1.5,
     letterSpacing: 0.15,
   );
 
   static TextStyle titleSmall(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 14),
+    fontSize: _scale(context, 14, 1.0),
     fontWeight: FontWeight.w500,
     height: 1.43,
     letterSpacing: 0.1,
   );
 
   static TextStyle bodyLarge(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 16),
+    fontSize: _scale(context, 16, 1.0),
     fontWeight: FontWeight.w400,
     height: 1.5,
     letterSpacing: 0.5,
   );
 
   static TextStyle bodyMedium(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 14),
+    fontSize: _scale(context, 14, 1.0),
     fontWeight: FontWeight.w400,
     height: 1.43,
     letterSpacing: 0.25,
   );
 
   static TextStyle bodySmall(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 12),
+    fontSize: _scale(context, 12, 1.0),
     fontWeight: FontWeight.w400,
     height: 1.33,
     letterSpacing: 0.4,
   );
 
   static TextStyle labelLarge(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 14),
+    fontSize: _scale(context, 14, 1.0),
     fontWeight: FontWeight.w500,
     height: 1.43,
     letterSpacing: 0.1,
   );
 
   static TextStyle labelMedium(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 12),
+    fontSize: _scale(context, 12, 1.0),
     fontWeight: FontWeight.w500,
     height: 1.33,
     letterSpacing: 0.5,
   );
 
   static TextStyle labelSmall(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 11),
+    fontSize: _scale(context, 11, 1.0),
     fontWeight: FontWeight.w500,
     height: 1.45,
     letterSpacing: 0.5,
   );
 
   static TextStyle timeDisplay(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 48),
+    fontSize: _scale(context, 48, 1.0),
     fontWeight: FontWeight.w400,
     height: 1.25,
     letterSpacing: 0.5,
   );
 
   static TextStyle dateDisplay(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 24),
+    fontSize: _scale(context, 24, 1.0),
     fontWeight: FontWeight.w400,
     height: 1.25,
     letterSpacing: 0.5,
   );
 
   static TextStyle meridianDisplay(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 16),
+    fontSize: _scale(context, 16, 1.0),
     fontWeight: FontWeight.w400,
     height: 1.25,
     letterSpacing: 0.5,
   );
 
   static TextStyle statValue(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 16),
+    fontSize: _scale(context, 16, 1.0),
     fontWeight: FontWeight.bold,
     height: 1.25,
     letterSpacing: 0.5,
   );
 
   static TextStyle statLabel(BuildContext context) => TextStyle(
-    fontSize: _scale(context, 8),
+    fontSize: _scale(context, 8, 1.0),
     fontWeight: FontWeight.w400,
     height: 1.25,
     letterSpacing: 0.5,
